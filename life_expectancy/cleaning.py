d89769def09474edfe71d491a50576a8515a75d3
Sara Paisana <anasarabp14@gmail.com>
2025-05-26 16:45:47 +0100

    Add GitHub Actions CI workflow with pytest and pylint

---
+++ life_expectancy/cleaning.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+def clean_data(region):
+    """Carregar o dataset e limpar os dados"""
+    df = pd.read_csv(
+    "/Users/sarapaisana/assignments/life_expectancy/data/eu_life_expectancy_raw.tsv", sep="\t")
+    #Separar a primeira coluna em 4 colunas novas
+    df[['unit', 'sex', 'age', 'region']] = df['unit,sex,age,geo\\time'].str.split(',', expand=True)
+
+    #Remover a coluna antiga
+    df = df.drop(columns=['unit,sex,age,geo\\time'])
+
+    #Converter os anos para formato longo
+    df = df.melt(
+        id_vars=['unit', 'sex', 'age', 'region'],
+        var_name='year',
+        value_name='value'
+    )
+
+    #Converter ano para inteiro
+    df['year'] = df['year'].astype(int)
+
+    #Filtrar apenas as linhas com valores numéricos válidos
+    df['value'] = df['value'].str.replace(r'[a-zA-Z]', '', regex=True).str.strip()
+    df['value'] = pd.to_numeric(df['value'], errors='coerce').astype(float)
+    df = df.dropna(subset=['value'])
+
+    # Filtrar apenas os dados de Portugal
+    df = df[df['region'] == 'PT']
+    return df
+
+
+
+if __name__ == "__main__": # pragma: no cover
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Limpa dados de esperança de vida da UE.")
+    parser.add_argument("--region", default="PT", help="Código do país (ex: PT, ES, FR)")
+    args = parser.parse_args()
+
+    df_cleaned = clean_data(region=args.region)
+
+    df_cleaned.to_csv(
+    "/Users/sarapaisana/assignments/life_expectancy/data/pt_life_expectancy.csv", index=False)
