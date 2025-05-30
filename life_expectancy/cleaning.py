f2ac74dab6b2c3746a4911a039cca59fe59482aa
Sara Paisana <anasarabp14@gmail.com>
2025-05-30 18:17:26 +0100

    Atualizar ficheiro data cleaning

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,37 +1,45 @@
 import pandas as pd
+from pathlib import Path
 
 def clean_data(region):
     """Carregar o dataset e limpar os dados"""
-    df = pd.read_csv(
-    "/Users/sarapaisana/assignments/life_expectancy/data/eu_life_expectancy_raw.tsv", sep="\t")
-    #Separar a primeira coluna em 4 colunas novas
+    base_path = Path(__file__).parent / "data"  # caminho relativo à pasta data
+    input_path = base_path / "eu_life_expectancy_raw.tsv"
+    output_path = base_path / f"{region.lower()}_life_expectancy.csv"
+
+    df = pd.read_csv(input_path, sep="\t")
+
+    # Separar a primeira coluna em 4 colunas novas
     df[['unit', 'sex', 'age', 'region']] = df['unit,sex,age,geo\\time'].str.split(',', expand=True)
 
-    #Remover a coluna antiga
+    # Remover a coluna antiga
     df = df.drop(columns=['unit,sex,age,geo\\time'])
 
-    #Converter os anos para formato longo
+    # Converter os anos para formato longo
     df = df.melt(
         id_vars=['unit', 'sex', 'age', 'region'],
         var_name='year',
         value_name='value'
     )
 
-    #Converter ano para inteiro
+    # Converter ano para inteiro
     df['year'] = df['year'].astype(int)
 
-    #Filtrar apenas as linhas com valores numéricos válidos
+    # Filtrar apenas as linhas com valores numéricos válidos
     df['value'] = df['value'].str.replace(r'[a-zA-Z]', '', regex=True).str.strip()
     df['value'] = pd.to_numeric(df['value'], errors='coerce').astype(float)
     df = df.dropna(subset=['value'])
 
-    # Filtrar apenas os dados de Portugal
-    df = df[df['region'] == 'PT']
+    # Filtrar apenas os dados da região passada como argumento
+    df = df[df['region'] == region]
+
+    # Guardar o dataframe limpo no ficheiro csv (output)
+    df.to_csv(output_path, index=False)
+
     return df
 
 
-
-if __name__ == "__main__": # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     import argparse
 
     parser = argparse.ArgumentParser(description="Limpa dados de esperança de vida da UE.")
@@ -39,6 +47,3 @@ if __name__ == "__main__": # pragma: no cover
     args = parser.parse_args()
 
     df_cleaned = clean_data(region=args.region)
-
-    df_cleaned.to_csv(
-    "/Users/sarapaisana/assignments/life_expectancy/data/pt_life_expectancy.csv", index=False)
